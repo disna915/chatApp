ccb77930b45a907f2d3613869609d2c61a68ffa8
Disna <disnathomas0@gmail.com>
2025-03-10 10:31:45 +0530

    firstchange

--- lib/controllers/chat_controller.dart
+++ lib/controllers/chat_controller.dart
@@ -8,12 +8,12 @@ class ChatController extends GetxController {
 
   RxList<Map<String, dynamic>> messages = <Map<String, dynamic>>[].obs;
 
-  // Function to get chat ID (combination of both UIDs)
+
   String getChatId(String user1, String user2) {
     return user1.hashCode <= user2.hashCode ? '${user1}_$user2' : '${user2}_$user1';
   }
 
-  // Fetch messages in real-time
+
   void loadMessages(String receiverId) {
     String chatId = getChatId(auth.currentUser!.uid, receiverId);
     firestore.collection('chats').doc(chatId).collection('messages')
@@ -24,7 +24,7 @@ class ChatController extends GetxController {
     });
   }
 
-  // Send message
+
   Future<void> sendMessage(String receiverId, String message) async {
     String chatId = getChatId(auth.currentUser!.uid, receiverId);
     Map<String, dynamic> messageData = {
